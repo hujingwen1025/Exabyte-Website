886ea224faf341d339a41c26d4ae1bb43892bec5
WilliamH <91361904+hujingwen1025@users.noreply.github.com>
2025-07-06 12:42:10 +0800

    Add Flask app for redirect with code parameter
    Introduces a new Flask application with a single route that redirects to a given URL, appending a 'code' query parameter. Adds Flask and gunicorn to requirements.txt.

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/mslcTransfer/<mslcURL>')
+def mslcTransfer(mslcURL):
+    try:
+        code = str(request.args.get('code'))
+        return redirect(str(mslcURL) + f'?code={code}')
+    except:
+        return "There was an error with the redirect URL. We don't know where to redirect you. Please return to the page you came from manually."
+
+if __name__ == '__main__':
+    app.run(debug=False, port=80, host='0.0.0.0')
