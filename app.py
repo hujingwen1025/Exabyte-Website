9a073cad2b55f4df3e04a72c6f6709eed6e80bd8
WilliamH <91361904+hujingwen1025@users.noreply.github.com>
2025-11-27 20:14:15 +0800

    Updated contents
    Added pages
    Added information
    Minor fixes

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask, request, redirect
+from flask import Flask, render_template, request, redirect
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/mission')
+def mission():
+    return render_template('mission.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 @app.route('/mslcTransfer/<path:mslcURL>')
 def mslcTransfer(mslcURL):
